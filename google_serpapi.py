87c01e6af5d81b367963bce61fad285d9357d516
Emre Kara <74061558+emrekara369@users.noreply.github.com>
2025-07-02 02:37:59 +0300

    Update google_serpapi.py

--- google_serpapi.py
+++ google_serpapi.py
@@ -1,109 +1,64 @@
+import base64
 import json
-import random
 import sys
 import time
-from urllib.parse import unquote
-import base64
 
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from xvfbwrapper import Xvfb
 
-def get_useragent():
+
+def send_req_with_selenium(query, start_date=None, end_date=None):
     """
-    Generates a random user agent string mimicking the format of various software versions.
-
-    The user agent string is composed of:
-    - Lynx version: Lynx/x.y.z where x is 2-3, y is 8-9, and z is 0-2
-    - libwww version: libwww-FM/x.y where x is 2-3 and y is 13-15
-    - SSL-MM version: SSL-MM/x.y where x is 1-2 and y is 3-5
-    - OpenSSL version: OpenSSL/x.y.z where x is 1-3, y is 0-4, and z is 0-9
-
-    Returns:
-        str: A randomly generated user agent string.
-    """
-    lynx_version = f"Lynx/{random.randint(2, 3)}.{random.randint(8, 9)}.{random.randint(0, 2)}"
-    libwww_version = f"libwww-FM/{random.randint(2, 3)}.{random.randint(13, 15)}"
-    ssl_mm_version = f"SSL-MM/{random.randint(1, 2)}.{random.randint(3, 5)}"
-    openssl_version = f"OpenSSL/{random.randint(1, 3)}.{random.randint(0, 4)}.{random.randint(0, 9)}"
-    return f"{lynx_version} {libwww_version} {ssl_mm_version} {openssl_version}"
-
-def send_req(query,start_date,end_date):
-    url = "https://www.google.com/search"
-    if start_date and end_date:
-        url += f"?tbs=cdr:1,cd_min:{start_date},cd_max:{end_date}"
-    elif start_date:
-        url += f"?tbs=cdr:1,cd_min:{start_date}"
-    elif end_date:
-        url += f"?tbs=cdr:1,cd_max:{end_date}"
-        
-    resp = requests.get(
-        url=url,
-        headers={
-            "User-Agent": get_useragent(),
-            "Accept": "*/*"
-        },
-        params={
-            "q": query,
-            "num": 500,
-            "hl": "en",
-            "start": 0,
-            "safe": "active",
-            "gl": None
-        },
-        proxies=None,
-        timeout=5,
-        verify=None,
-        cookies = {
-            'CONSENT': 'PENDING+987', # Bypasses the consent page
-            'SOCS': 'CAESHAgBEhIaAB',
-        }
-    )
-    resp.raise_for_status()
-    return resp
-
-def fetch_urls(query, start_date=None, end_date=None):
-    """
-    Fetches URLs from Google search results for a given query.
-
+    Sends a search request to Google using Selenium to bypass bot detection.
+    
     Args:
         query (str): The search query.
         start_date (str, optional): The start date for filtering search results. Defaults to None.
         end_date (str, optional): The end date for filtering search results. Defaults to None.
-
+    
     Returns:
-        list: A list of unique URLs fetched from the search results.
+        requests.Response: The response object containing the search results.
     """
-    start = 0
-    fetched_results = 0  # Keep track of the total fetched results
-    fetched_links = set()  # to keep track of links that are already seen previously
-    while fetched_results < 500:
-        resp = send_req(query, start_date, end_date)
-        soup = BeautifulSoup(resp.text, "html.parser")
-        result_block = soup.find_all("div", class_="ezO2md")
-        new_results = 0  # Keep track of new results in this iteration
+    results = []
+    with Xvfb(width=1920, height=1080, colordepth=24):
+        options = Options()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--window-size=1920,1080")
+        try:
+            driver = webdriver.Chrome(options=options)
 
-        for result in result_block:
-            # Find the link tag within the result block
-            link_tag = result.find("a", href=True)
-            # Extract and decode the link URL
-            link = unquote(link_tag["href"].split("&")[0].replace("/url?q=", "")) if link_tag else ""
-            # Check if the link has already been fetched and if unique results are required
-            if link in fetched_links or not link.startswith("http"):
-                continue  # Skip this result if the link is not unique
-            # Add the link to the set of fetched links
-            fetched_links.add(link)
-            fetched_results += 1
-            # Increment the count of new results in this iteration
-            new_results += 1
-            if fetched_results >= 500:
-                break  # Stop if we have fetched the desired number of results
+            driver.get("https://www.google.com/")
+            time.sleep(2)
 
-        if new_results == 0:
-            break  # Break the loop if no new results were found in this iteration
+            try:
+                driver.find_elements(By.TAG_NAME,"button")[4].click()
+                time.sleep(1)
+            except:
+                pass
 
-        start += 10  # Prepare for the next set of results
-        time.sleep(2)
-    return list(fetched_links)
+            search_bar = driver.find_element(By.TAG_NAME,"textarea")
+            search_bar.send_keys(query)
+            search_bar.submit()
+            time.sleep(4)
+            search_result_div = driver.find_element(By.XPATH,'//div[@id="search"]')
+            results.extend([_.get_attribute("href") for _ in search_result_div.find_elements(By.TAG_NAME,"a")])
+            try:
+                page_number = len(driver.find_element(By.TAG_NAME,"tbody").find_elements(By.TAG_NAME,"td"))-2
+            except:
+                page_number = 1
+            print(page_number)
+            for page in range(1,page_number):
+                driver.get(driver.current_url+"&start="+str(page*10))
+                time.sleep(2)
+                search_result_div = driver.find_element(By.XPATH,'//div[@id="search"]')
+                results.extend([_.get_attribute("href") for _ in search_result_div.find_elements(By.TAG_NAME,"a")])
+        except:
+            driver.quit()
+        return results
 
 def search(queries, start_date=None, end_date=None):
     """
@@ -119,10 +74,9 @@ def search(queries, start_date=None, end_date=None):
     """
     results = []
     for query in queries:
-        print("Received:", query)
-        results.extend(fetch_urls(query, start_date, end_date))
-        print("Total results", len(results))
+        results.extend(send_req_with_selenium(query, start_date, end_date))
         time.sleep(5)
+    results = list(set(results))
     with open("/home/ubuntu/results.txt","w") as f:
         f.write("\n".join(results))
 
